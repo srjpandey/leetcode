c08b3f894c7345f6fbfb4bb173c226c99347f678
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-09-17 09:35:19 +0545

    Time: 63 ms (79.65%), Space: 51.8 MB (28.2%) - LeetHub

---
+++ 2859-add-two-promises/2859-add-two-promises.ts
@@ -0,0 +1,8 @@
+type P = Promise<number>
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+    return await Promise.all([promise1, promise2]).then (
+        ([val1,val2])=> val1+val2
+    );
+};
+
