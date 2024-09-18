a928130835bc8140dc780631414ee9bacfa1f9ff
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-09-18 09:37:40 +0545

    Time: 73 ms (40.03%), Space: 52 MB (10.45%) - LeetHub

--- 2821-timeout-cancellation/2821-timeout-cancellation.ts
+++ 2821-timeout-cancellation/2821-timeout-cancellation.ts
@@ -1,35 +1,11 @@
-/**
- * @param {Function} fn
- * @param {Array} args
- * @param {number} t
- * @return {Function}
- */
-const cancellable = function(fn, args, t) {
-    // cancelFn function//
-    const cancelFn = function (){
-      clearTimeout(timer);
-  };
-  const timer = setTimeout(()=>{
-      fn(...args)
+var cancellable = function(fn, args, t) {
+  const timeoutId = setTimeout(function() {
+    fn.apply(null, args);
   }, t);
-  return cancelFn ;
+
+  const cancelFn = function() {
+    clearTimeout(timeoutId);
+  };
+
+  return cancelFn;
 };
-
-
-/**
- *  const result = []
- *
- *  const fn = (x) => x * 5
- *  const args = [2], t = 20, cancelT = 50
- *
- *  const log = (...argsArr) => {
- *      result.push(fn(...argsArr))
- *  }
- *       
- *  const cancel = cancellable(fn, args, t);
- *           
- *  setTimeout(() => {
- *     cancel()
- *     console.log(result) // [{"time":20,"returned":10}]
- *  }, cancelT)
- */
