3f3ec84f464bdc2417737ad81ff0ba7502f0e9e4
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-11-07 09:44:50 +0545

    Time: 4 ms (36.25%), Space: 59.4 MB (63.39%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.js
@@ -0,0 +1,18 @@
+var canCompleteCircuit = function(gas, cost) {
+    if (gas.reduce((a, b) => a + b, 0) < cost.reduce((a, b) => a + b, 0)) {
+        return -1;
+    }
+
+    let currentGas = 0;
+    let start = 0;
+
+    for (let i = 0; i < gas.length; i++) {
+        currentGas += gas[i] - cost[i];
+        if (currentGas < 0) {
+            currentGas = 0;
+            start = i + 1;
+        }
+    }
+
+    return start;    
+};
