6d0133cfdbcef3b84e80f46929d0d19a697818f8
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-11-09 17:22:04 +0545

    Time: 0 ms (100%), Space: 51 MB (41.71%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,12 @@
+var reverseWords = function(s) {
+    let words = s.split(' ');
+    let res = [];
+
+    for (let i = words.length - 1; i >= 0; i--) {
+        if (words[i]) {
+            res.push(words[i]);
+        }
+    }
+
+    return res.join(' ');
+};
