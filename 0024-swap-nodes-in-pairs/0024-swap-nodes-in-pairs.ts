78c9138b0d63d7b90e0f18942898dca1419caf9a
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-11-12 09:26:13 +0545

    Time: 0 ms (100%), Space: 51.2 MB (89.61%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swapPairs(head: ListNode | null): ListNode | null {
+    if (!head) return null;
+
+    const second = head.next;
+    if (!second) return head;
+
+    head.next = swapPairs(second.next);
+    second.next = head;
+
+    return second;
+}
