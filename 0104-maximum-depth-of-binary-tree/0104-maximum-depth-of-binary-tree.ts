89308e6844a0655c1a161b912dcb11d6954d9b18
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-11-18 14:41:12 +0545

    Time: 3 ms (13.83%), Space: 53.5 MB (92.14%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+const maxDepth = (root: TreeNode | null): number => root ? Math.max(maxDepth(root.left), maxDepth(root.right)) + 1 : 0
