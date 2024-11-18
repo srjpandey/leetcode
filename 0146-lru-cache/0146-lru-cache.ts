f63b64f9a60de7a2c11ad0f64d5d38782eb78c35
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-11-18 10:00:14 +0545

    Time: 109 ms (61.2%), Space: 106.2 MB (60.93%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.ts
@@ -0,0 +1,34 @@
+class LRUCache {
+  capacity: number;
+  map: Map<number, number>;
+
+  constructor(capacity: number) {
+      this.capacity = capacity;
+      this.map = new Map();
+  }
+
+  get(key: number): number {
+    const value = this.map.get(key);
+
+    if (value === undefined) return -1;
+          
+    // Small hack to re-order keys: we remove the requested key and place it at the end
+    this.map.delete(key);
+    this.map.set(key, value);
+    
+    return value;
+  }
+
+  put(key: number, value: number): void {
+    // remove last element to avoid overflow, only if it does not have 
+    // the inserted key is a new key
+    if (this.map.size >= this.capacity && !this.map.has(key)) {
+        const firstKey = this.map.keys().next().value;
+        this.map.delete(firstKey);
+    }
+    
+    // Small hack to re-order keys: we remove the requested key and place it at the end
+    this.map.delete(key);
+    this.map.set(key, value);
+  }
+}
