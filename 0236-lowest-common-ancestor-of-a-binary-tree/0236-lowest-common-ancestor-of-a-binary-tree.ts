7b1fb9a49c8ee2122ea0df01f41015b4c345a0f3
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-11-27 09:48:48 +0545

    Time: 81 ms (26.89%), Space: 59.9 MB (38%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.ts
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.ts
@@ -1,11 +1,28 @@
-function lowestCommonAncestor(root: TreeNode | null, p: TreeNode, q: TreeNode): TreeNode | null{
-  if (root === null) return null;
-  
-  const leftHasPQ = lowestCommonAncestor(root.left, p, q);
-  const rightHasPQ = lowestCommonAncestor(root.right, p, q);
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
 
-  if ((leftHasPQ && rightHasPQ) || (root.val === p.val || root.val === q.val))
-    return root;
+function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
+    if (root === null || root === p || root === q) {
+        return root
+    }
 
-  return leftHasPQ || rightHasPQ;
+    const left = lowestCommonAncestor(root.left, p, q)
+    const right = lowestCommonAncestor(root.right, p, q)
+
+    if (left !== null && right !== null) {
+        return root
+    }
+
+    return left !== null ? left : right
 };
