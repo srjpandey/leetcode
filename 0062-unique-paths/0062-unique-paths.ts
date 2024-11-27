b0396407a1bd8f9f73bc0c8594e80d9fb78b9b00
Suraj Pandey <80386591+srjpandey@users.noreply.github.com>
2024-11-27 09:54:52 +0545

    Time: 0 ms (100%), Space: 52.1 MB (24.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.ts
+++ 0062-unique-paths/0062-unique-paths.ts
@@ -1,31 +1,16 @@
 function uniquePaths(m: number, n: number): number {
-    let memo = Array(m).fill(0).map(() => Array(n).fill(0))
-
-    return sumPossiblePaths(m - 1, n - 1, memo)
-};
-
-
-const sumPossiblePaths = (row: number, column: number, memo: number[][] = []): number => {
-    if (row === 0 && column === 0) {
-        return 1
+    const grid = [];
+    for(let i = 0; i < m; i++) {
+        grid[i] = [];
+        for(let j = 0; j < n; j++) {
+            grid[i][j] = 1;
+        }
     }
     
-    let leftWay = 0
-    let topWay = 0
-
-    if (row > 0) {
-        if (!memo[row - 1][column]) {
-            memo[row - 1][column] = sumPossiblePaths(row - 1, column, memo)
+    for(let i = 1; i < m; i++) {
+        for(let j = 1; j < n; j++) {
+            grid[i][j] = grid[i-1][j] + grid[i][j-1];
         }
-        topWay = memo[row - 1][column]
     }
-    if (column > 0) {
-        if (!memo[row][column - 1]) {
-            memo[row][column - 1] = sumPossiblePaths(row, column - 1, memo)
-        }
-        leftWay = memo[row][column - 1]
-    }
-    memo[row][column] = leftWay + topWay 
-
-    return memo[row][column]
-}
+    return grid[m-1][n-1];   
+};
